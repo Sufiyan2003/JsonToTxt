16599f46674ec4b4fb41d11f9faf7bd732dadf4e
Muhammad Sufiyan Sadiq <118098066+Sufiyan2003@users.noreply.github.com>
2023-12-03 19:14:56 +0500

    Add files via upload
    you can now make subfolders inside of the images and according to the number of classes you have and it will filter out the text files to those exact folders.

--- jsonToTxt.py
+++ jsonToTxt.py
@@ -4,14 +4,9 @@ import os
 #Author: Muhammad Sufiyan Sadiq
 
 # add your path to your json files here
-folder_path = "../path/to/your/json/file"
-target_path = "../path/to/your/txt/file"
+main_folder_path = "C:/Users/Fahad/Desktop/python Code/jsonToTxt/jsons"
+main_target_path = "C:/Users/Fahad/Desktop/python Code/jsonToTxt/text"
 
-Jsons_list = os.listdir(folder_path)
-number_of_jsons = len(Jsons_list)
-
-
-number_of_classes = 5
 
 # user-defined class mapping
 class_mapping = {
@@ -20,53 +15,63 @@ class_mapping = {
     # Add more classes as needed
 }
 
+# we also need to get the number of folders present in this directory
+folders = os.listdir(main_folder_path)
+print(folders)
+for folder in folders:
+    folder_path = os.path.join(main_folder_path, folder) 
+    Json_list = os.listdir(folder_path)
+    number_of_jsons = len(Json_list)
+    for i in range(number_of_jsons):
+        new_path  = os.path.join(folder_path, Json_list[i])
+        with open(new_path, 'r') as json_file:
+            data = json.load(json_file)
+            
+            # find the length and width of the image
+            height = data["imageHeight"]
+            width = data["imageWidth"]
 
-for i in range(number_of_jsons):
-    new_path  = os.path.join(folder_path, Jsons_list[i])
-    with open(new_path, 'r') as json_file:
-        data = json.load(json_file)
-        
-        # find the length and width of the image
-        height = data["imageHeight"]
-        width = data["imageWidth"]
-
-        yolo_format_string = ""        
-        number_of_boxes = len(data["shapes"])
-        for i in range(number_of_boxes):
-            box_data = data["shapes"][i]
-            # getting the class id
-            try:
-                Class = class_mapping[box_data["label"]]    
+            yolo_format_string = ""        
+            number_of_boxes = len(data["shapes"])
+            for i in range(number_of_boxes):
+                box_data = data["shapes"][i]
+                # getting the class id
+                try:
+                    Class = class_mapping[box_data["label"]]    
+                
+                except:
+                    print("Unknown class label found please add this class in your python file and try again")
+                    break
+                
+                # get the points array
+                points_array = box_data["points"]
+                
+                # get the points from the array
+                x_coordinate1 = points_array[0][0]
+                x_coordinate2 = points_array[1][0]
+                y_coordinate1 = points_array[0][1]
+                y_coordinate2 = points_array[1][1]
+                
+                # get the box width and height
+                box_height = (y_coordinate2 -y_coordinate1) / height
+                box_width = (x_coordinate2 - x_coordinate1) / width
+                
+                x_center = (x_coordinate1 + (x_coordinate2 - x_coordinate1) / 2) / width
+                y_center = (y_coordinate1 + (y_coordinate2 - y_coordinate1) / 2) / height
+                
+                yolo_format_string  = yolo_format_string + f"{Class} {x_center} {y_center} {box_width} {box_height}\n"
+                # end of for loop
             
-            except:
-                print("Unknown class found please add this class in your python file and try again")
-                break
+            # this is the yolo format paragraph that can be written inside of our text file
+            yolo_format_string = yolo_format_string.rstrip('\n')
             
-            # get the points array
-            points_array = box_data["points"]
             
-            # get the points from the array
-            x_coordinate1 = points_array[0][0]
-            x_coordinate2 = points_array[1][0]
-            y_coordinate1 = points_array[0][1]
-            y_coordinate2 = points_array[1][1]
+            file_name = data["imagePath"]
+            file_name= os.path.basename(file_name)
+            file_name = os.path.splitext(file_name)[0] + '.txt'  # convert it to txt file
             
-            # get the box width and height
-            box_height = (y_coordinate2 -y_coordinate1) / height
-            box_width = (x_coordinate2 - x_coordinate1) / width
             
-            x_center = (x_coordinate1 + (x_coordinate2 - x_coordinate1) / 2) / width
-            y_center = (y_coordinate1 + (y_coordinate2 - y_coordinate1) / 2) / height
-            
-            yolo_format_string  = yolo_format_string + f"{Class} {x_center} {y_center} {box_width} {box_height}\n"
-            # end of for loop
-        
-        yolo_format_string = yolo_format_string.rstrip('\n')
-        
-        
-        file_name = data["imagePath"]
-        file_name= os.path.basename(file_name)
-        file_name = os.path.splitext(file_name)[0] + '.txt'
-        target_file_path = os.path.join(target_path, file_name)
-        with open(target_file_path, 'w') as writeFile:
-            writeFile.write(yolo_format_string)
+            temp_target_path = os.path.join(main_target_path, folder)
+            target_file_path = os.path.join(temp_target_path, file_name)
+            with open(target_file_path, 'w') as writeFile:
+                writeFile.write(yolo_format_string)
