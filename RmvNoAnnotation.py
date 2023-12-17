8fc63cfb8dbf6a34a0b6b9a42788cdb1245849ad
Muhammad Sufiyan Sadiq <118098066+Sufiyan2003@users.noreply.github.com>
2023-12-18 01:41:12 +0500

    Update RmvNoAnnotation.py
    the code now removes the extensions and thus can be used with any file type

--- RmvNoAnnotation.py
+++ RmvNoAnnotation.py
@@ -1,19 +1,32 @@
 import os
 
 
+# This is to remove any file extension in case you want to use it for any type of file
+# if you want to filter out files of different types then you will need to edit this code
+def RemoveFileExtension(FileName):
+    position = FileName.find('.')
+    OnlyFileName = FileName[:position]
+    return OnlyFileName
+
+
+def RemoveFileExtensionForArray(FileArray):
+    for i in range(len(FileArray)):
+        FileArray[i] = RemoveFileExtension(FileArray[i])
+    return FileArray
+
 # this path contains the bigger set of files which we need to filter
-path = "/path/to/target"
+path = "path/to/target"
 
 # this contais the files that are to remain
-to_remain = "/path/to/lookup"
+to_remain = "path/to/lookup"
 
 
 # need to check if the name is present in that file or not
-target_files = os.listdir(path)
-lookup_files = os.listdir(to_remain)
+target_files = RemoveFileExtensionForArray(os.listdir(path))
+lookup_files = RemoveFileExtensionForArray(os.listdir(to_remain))
 
 for i in range(len(target_files)):
-    check_file = target_files[i]
+    check_file = (target_files[i])
     print(check_file)
     if check_file not in lookup_files:
         path_to_file = os.path.join(path, check_file)
